75283a3b4eb7829ea5ecfba33306fd6ca5176825
Nitesh Jha <niteshj818@gmail.com>
2020-09-11 00:48:24 +0530

    initial feature descriptor

---
+++ OpenCV_basics/Image_Descriptors/findCorners.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+
+img=cv2.imread('chess.PNG')
+imggray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+imggray=np.float32(imggray)
+corners=cv2.cornerHarris(imggray,5,29,0.1)
+print(corners[:10])
+img[corners>0.1*corners.max()]=[0,0,255]
+cv2.imshow('',img)
+cv2.waitKey()
+cv2.destroyAllWindows()
